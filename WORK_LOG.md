782fa1651c6065f44af58d62ca42157eb6cabc35
adokanchi <tony.dokanchi@menloschool.org>
2024-09-04 12:20:45 -0700

    9/4/24 12pm

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,14 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date  |      Time      |                                                                                                                 Update |
+|:------|:--------------:|-----------------------------------------------------------------------------------------------------------------------:|
+| Sep 3 | 9:30-10:30 PM  |          I tried an approach where I had a recursive helper method that takes in a start cell and tells me how long it |
+|       |                | would take to get to the end of the board from that cell. That approach didn't really work the way I wanted it to, but |
+| Sep 4 | 10:00-10:40 PM |                                                                    it did help me understand the problem a lot better. |
+|       |                |                         I ditched the helper method approach and instead did something similar to the mazesolvers from |
+|       |                |                  last year, where I had an ArrayList of all the seen cells and all the "currently exploring" cells and |
+|       |                |                         used those to find new cells to explore. I'm now passing 4/4 tests and have done a tiny little |
+|       |                |                                                                                       bit of optimizing, but not much. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -4,18 +4,68 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Tony Dokanchi
  *
  */
 
+import java.util.ArrayList;
+
 public class MokshaPatam {
-
-    /**
-     * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
-     *  to reach the final square on a board with the given size, ladders, and snakes.
-     */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        int numRolls = 0;
 
-        return 0;
+        // foundCells = all cells visited, ever
+        // oldCells = the start points of the next time step
+        // newCElls = the end points of the next time step
+        ArrayList<Integer> foundCells = new ArrayList<Integer>();
+        ArrayList<Integer> oldCells = new ArrayList<Integer>();
+        ArrayList<Integer> newCells = new ArrayList<Integer>();
+
+        // We start on cell 1
+        oldCells.add(1);
+        foundCells.add(1);
+
+        while (numRolls < boardsize + 10) { // Repeat until we're sure there is no solution
+            numRolls++;
+            for (int roll = 1; roll <= 6; roll++) {
+                for (int oldCell : oldCells) {
+                    // Move from oldCell to newCell
+                    int newCell = oldCell + roll;
+                    while (newCell != followPath(newCell, ladders, snakes)) {
+                        newCell = followPath(newCell, ladders, snakes);
+                    }
+
+                    // If the goal is reached
+                    if (newCell == boardsize) {
+                        return numRolls;
+                    }
+
+                    // If newCell hasn't been reached yet
+                    if (!foundCells.contains(newCell)) {
+                        newCells.add(newCell);
+                        foundCells.add(newCell);
+                    }
+                }
+            }
+            // Next roll
+            oldCells = newCells;
+            newCells = new ArrayList<Integer>();
+        }
+        return -1;
+    }
+
+    // Takes in a startCell and the corresponding paths. Returns the cell that startCell leads to.
+    public static int followPath(int startCell, int[][] ladders, int[][] snakes) {
+        for (int[] ladder : ladders) {
+            if (startCell == ladder[0]) {
+                return ladder[1];
+            }
+        }
+        for (int[] snake : snakes) {
+            if (startCell == snake[0]) {
+                return snake[1];
+            }
+        }
+        return startCell;
     }
 }
